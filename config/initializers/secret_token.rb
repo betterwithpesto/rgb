5cdf4a876f0398e54231382ec4c3ae1c434abc06
Patrick Lu <patlu08@gmail.com>
2012-12-13 13:47:54 -0600

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Rgb7::Application.config.secret_token = 'c2d3f531ea4acd80083bd7e4492a3d04b9cf3eedfa7f11bc7b447d5b1b5eb24ccfdabf9b4a79040e2a8bd149114b9e04c14d996b5eeb5194b59654a970a6debc'
---
+++ config/routes.rb
@@ -0,0 +1,3 @@
+Rgb7::Application.routes.draw do
+  root :to => "home#index"
+end

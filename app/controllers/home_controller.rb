fcb7f0a5889e68f0db8b88bf531c75f1ceb6831e
Patrick Lu <patlu08@gmail.com>
2012-12-13 14:14:16 -0600

    trying to display pictures

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,6 @@
 class HomeController < ApplicationController
 	def index
+		person = Flickr.people.find("25777376@N05")
+    	@photos = person.get_public_photos(sizes: :all).first(10)
 	end
 end
---
+++ config/initializers/flickr-objects.rb
@@ -0,0 +1,4 @@
+Flickr.configure do |config|
+  config.api_key = "1a6a908e1a7fb467a461956216f24243"
+  config.shared_secret = "ab9257802ff21d26"
+end

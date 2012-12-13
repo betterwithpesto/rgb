d9e024b072c914e56b079e5d5f495bcb7b3038cb
Patrick Lu <patlu08@gmail.com>
2012-12-13 15:05:04 -0600

    displaying images

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,6 @@
 class HomeController < ApplicationController
 	def index
 		person = Flickr.people.find("25777376@N05")
-    	@photos = person.get_public_photos(sizes: :all).first(10)
+    	@photos = person.get_public_photos(sizes: :all).map(&:medium500!)
 	end
 end

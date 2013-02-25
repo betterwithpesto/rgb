37e1774581cc448c44afb4e4b5b897f386c7fe5a
Patrick Lu <patlu08@gmail.com>
2013-02-25 14:19:22 -0600

    redid login

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,6 +1,6 @@
 # Be sure to restart your server when you modify this file.
 
-Rgb7::Application.config.session_store :cookie_store, key: '_rgb7_session', :domain => 'rgb7.dev'
+Rgb7::Application.config.session_store :cookie_store, key: '_rgb7_session', :domain => 'rgb.io'
 
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information

6a0390a78d997f6bcfe06a1d0a1f71b79889daf4
Patrick Lu <patlu08@gmail.com>
2013-02-25 20:32:07 -0600

    change cookie to react to local and production

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,6 +1,6 @@
 # Be sure to restart your server when you modify this file.
 
-Rgb7::Application.config.session_store :cookie_store, key: '_rgb7_session', :domain => 'rgb.io'
+Rgb7::Application.config.session_store :cookie_store, key: '_rgb7_session', :domain => :all
 
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information

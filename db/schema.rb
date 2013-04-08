097fd9265ada86ea93bb674ba2da58cf55c1eda5
Patrick Lu <patlu08@gmail.com>
2013-04-07 19:39:50 -0500

    links and limit

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130321030219) do
+ActiveRecord::Schema.define(:version => 20130403042631) do
 
   create_table "authorizations", :force => true do |t|
     t.string   "provider"
@@ -39,6 +39,7 @@ ActiveRecord::Schema.define(:version => 20130321030219) do
     t.datetime "updated_at", :null => false
     t.string   "urlname"
     t.string   "about"
+    t.integer  "limit"
   end
 
 end

7f7c69b69ce478e8d83880c720010d8267fe9af0
Patrick Lu <patlu08@gmail.com>
2013-04-07 21:04:45 -0500

    adding files

--- app/controllers/links_controller.rb
+++ app/controllers/links_controller.rb
@@ -1,9 +1,10 @@
 class LinksController < ApplicationController
-def create
-	@link = Link.new(params[:link])
-	if @link.save
-		redirect_to edit_url(subdomain: current_user.urlname)
-	else
-		redirect_to edit_url(subdomain: current_user.urlname)
+	def create
+		@link = Link.new(params[:link])
+		if @link.save
+			redirect_to edit_url(subdomain: current_user.urlname)
+		else
+			redirect_to edit_url(subdomain: current_user.urlname)
+		end
 	end
 end

faa3a4b1ef03025c25adb91d449661c39ebdbe84
Patrick Lu <patlu08@gmail.com>
2013-04-07 20:45:53 -0500

    link logic

--- app/controllers/links_controller.rb
+++ app/controllers/links_controller.rb
@@ -1,6 +1,6 @@
 class LinksController < ApplicationController
 def create
-	@link = Link.new({params[:user_id], params[:name], params[:url]})
+	@link = Link.new(params[:link])
 	if @link.save
 		redirect_to edit_url(subdomain: current_user.urlname)
 	else
